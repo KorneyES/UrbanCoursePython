3cc5ee71139b32dce2fdc1317cd6bdb26381f0a4
KorneyES <korney100500@gmail.com>
2024-08-30 09:32:59 +0400

    Add files via upload

--- module_6_1.py
+++ module_6_1.py
@@ -19,36 +19,41 @@ class Animal:
         self.fed = False
         self.name = name
 
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+
 class Plant:
     def __init__(self, name):
         self.edible = False
         self.name = name
 
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
+    # def eat(self, food):
+    #     if food.edible:
+    #         print(f'{self.name} съел {food.name}')
+    #         self.fed = True
+    #     else:
+    #         print(f'{self.name} не стал есть {food.name}')
+    #         self.alive = False
 
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
+    pass
+
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
+    pass
+    # def eat(self, food):
+    #     if food.edible:
+    #         print(f'{self.name} съел {food.name}')
+    #         self.fed = True
+    #     else:
+    #         print(f'{self.name} не стал есть {food.name}')
+    #         self.alive = False
 
 class Flower(Plant):
     pass
